f372225e54fa55276f1975b0b53ddb06ecc15a34
Ketan Mehta <45426198+ketanmehta244241@users.noreply.github.com>
2019-07-18 19:43:31 +0530

    Create stack important.java

---
+++ Stack and Queue/src/stack important.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+Methods in Stack class
+
+1 Object push(Object element) : Pushes an element on the top of the stack.
+2 Object pop() : Removes and returns the top element of the stack. An ‘EmptyStackException’ exception is thrown if we call pop() when the invoking stack is empty.
+3 Object peek() : Returns the element on the top of the stack, but does not remove it.
+4 boolean empty() : It returns true if nothing is on the top of the stack. Else, returns false.
+5 int search(Object element) : It determines whether an object exists in the stack. If the element is found, it returns the 
+                               position of the element from the top of the stack. Else, it returns -1.
