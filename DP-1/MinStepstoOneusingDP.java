f4a89de18522d99fcb1b1e918af3942bf8dede60
Ketan Mehta <45426198+ketanmehta244241@users.noreply.github.com>
2019-07-25 23:05:49 +0530

    Update MinStepstoOneusingDP.java

--- DP-1/MinStepstoOneusingDP.java
+++ DP-1/MinStepstoOneusingDP.java
@@ -1,9 +1,12 @@
+// this is the bottom up_______ approch which is better than recurtion 
+//because we have to think more in recurtion 
 public class Solution { 
     public static int countStepsTo1(int n){
         int steps[] = new int[n + 1]; 
         for(int i = 2; i <= n; i++){
             int count1,count2 = Integer.MAX_VALUE, count3 = Integer.MAX_VALUE; 
             count1 = steps[i - 1]; 
+            // count1 is the steps of back element
             if(i % 2 == 0){
                 count2 = steps[i / 2]; 
             }
