7a23c533a178a98bd8abcb41afbb001cef69221f
Ketan Mehta <45426198+ketanmehta244241@users.noreply.github.com>
2019-08-04 01:52:07 +0530

    Create  GetPathBFS.java

---
+++ Graphs/GetPathBFS.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class Solution{
+     public static void getpath(int edges[][],int start,int end,boolean visited[],HashMap<Integer, Integer> map){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start); 
+        int n=edges.length;
+        int e=0;
+        while(q.size()!=0){
+            int nekal=q.remove();
+            visited[nekal]=true;
+            // System.out.print(nekal+" ");
+            for(int i=0;i<n;i++){
+                if(edges[nekal][i]==1&&!visited[i]){
+                    map.put(i,nekal);
+                    // the map is created and it will tell the parent of the child which is putted in the map
+                    visited[i]=true;
+                    q.add(i);
+                    if(i==end){
+                      e=1;    
+                    }
+                    if(e==1){
+                        break;
+                    }
+                }
+              if(e==1){
+                break;
+              } 
+            }
+        }
+         if(e==1){
+             int last=map.get(end);
+            System.out.print(end+" ");
+             while(last!=end){
+                 // what's the proper condition for while loop to end
+                 System.out.print(last+" ");
+                 if(map.get(last)!=null)
+                 last=map.get(last);
+             }            
+         }  
+     }
+    public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+        // total number of edges e
+		int e = s.nextInt();
+        int edges[][]=new int[n][n];
+        for(int i=0;i<e;i++){
+		// fv=first vertice
+		// start=second vertice
+            int fv=s.nextInt();
+            int sv=s.nextInt();
+            edges[fv][sv]=1;
+            edges[sv][fv]=1;
+        }
+        int start=s.nextInt();
+        int end=  s.nextInt();    
+         HashMap<Integer, Integer> max = new HashMap<>();
+        boolean visited[]= new boolean[edges[0].length];
+        getpath(edges,start,end,visited,max);
+    }
+}
