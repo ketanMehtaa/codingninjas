b7a693e2a6dd3cc37ef7ddeb2c03774dd3312d5f
Ketan Mehta <45426198+ketanmehta244241@users.noreply.github.com>
2019-08-06 03:35:16 +0530

    Update LongestCommonSubsequenceDP.java

--- DP-2/LongestCommonSubsequenceDP.java
+++ DP-2/LongestCommonSubsequenceDP.java
@@ -6,12 +6,12 @@ public class LCS {
 		for(int i=0;i<=m;i++){
            storage[i][0]=0;
         }
-        for(int i=0;i<=m;i++){
+        for(int i=0;i<=n;i++){
            storage[0][i]=0;
         }
           for(int i=1;i<=m;i++){
              for(int j=1;j<=n;j++){
-                if(s.charAt(m-i)==t.charAt(n-j){
+                if(s.charAt(m-i)==t.charAt(n-j)){
                    storage[i][j]=1+storage[i-1][j-1];
                 }else{
                    storage[i][j]=Math.max(storage[i][j-1],storage[i-1][j]);
