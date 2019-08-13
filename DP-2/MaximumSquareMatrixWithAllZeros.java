d9c9b56848448e1580f85b36f48e96eff11dc288
Ketan Mehta <45426198+ketanmehta244241@users.noreply.github.com>
2019-08-14 03:20:31 +0530

    Update MaximumSquareMatrixWithAllZeros.java

--- DP-2/MaximumSquareMatrixWithAllZeros.java
+++ DP-2/MaximumSquareMatrixWithAllZeros.java
@@ -4,6 +4,8 @@ public class Solution {
 	public static int findMaxSquareWithAllZeros(int[][] input){
       int storage[][]=new int[input.length][input[0].length];
         int ans=0; 
+	// first fill the rows 
+	// if in input array ,at that place if it is 1 then put 0 in storage array because there is no matrix required for zeros
         for(int i=0;i<input.length;i++){
             if(input[i][0]==0){
                 storage[i][0]=1;
@@ -11,6 +13,8 @@ public class Solution {
             }
             else storage[i][0]=0;
         }
+       // second fill the colns 
+       // if in input array ,at that place if it is 1 then put 0 in storage array because there is no matrix required for zeros
         for(int i=0;i<input[0].length;i++){
             if(input[0][i]==0){
                  storage[0][i]=1;
@@ -18,7 +22,12 @@ public class Solution {
             }
             else storage[0][i]=0;
         } 
-       
+       //1 2 
+       //3 ?
+       // now fill this with min of 1,2,3  + 1
+        // and do this to all -----
+	//	?=2;
+	// so fill your full array in this order	
         for(int i=1;i<input.length;i++){
             for(int j=1;j<input[0].length;j++){
                 if(input[i][j]==1){
